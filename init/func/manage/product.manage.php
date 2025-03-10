77c33df10fc0e0cd565c73003f0306e5f8dbcfa9
Chisae <chisae0123@gmail.com>
2025-03-10 23:43:33 +0700

    week 10

--- init/func/manage/product.manage.php
+++ init/func/manage/product.manage.php
@@ -30,33 +30,7 @@ function productSlugExists($slug)
 
 function createProduct($name, $slug, $price, $short_des, $long_des, $image, $id_categories)
 {
-    $img_name = $image['name'];
-    $img_size = $image['size'];
-    $tmp_name = $image['tmp_name'];
-    $error = $image['error'];
-
-    $dir = './assets/images/';
-
-    $allow_exs = ['pjg', 'png', 'pjeg'];
-    if ($error !== 0) {
-        throw new Exception('Unknown error occurred!');
-    }
-
-    if ($img_size > 500000) {
-        throw new Exception('File size is too large!');
-    }
-
-    $image_ex = pathinfo($img_name, PATHINFO_EXTENSION);
-    $image_lowercase_ex = strtolower($image_ex);
-
-    if (!in_array($image_lowercase_ex, $allow_exs)) {
-        throw new Exception('File extension is not allowed!');
-    }
-
-    $new_image_name = uniqid("PI-") . '.' . $image_lowercase_ex;
-    $image_path = $dir . $new_image_name;
-    move_uploaded_file($tmp_name, $image_path);
-
+    $image_path = uploadProductImage($image);
     global $db;
     $db->begin_transaction();
     try {
@@ -100,3 +74,105 @@ function deleteProduct($id)
     return false;
 }
 
+// function getProductCategories($id)
+// {
+//     //1 => [1, 2, 3];
+//     global $db;
+//     $query = $db->query("SELECT id_category FROM tbl_product_category WHERE id_product = '$id'");
+//     if ($query->num_rows) {
+//         while ($row = $query->fetch_object()) {
+//             $arr[] = $row->id_category;
+//         }
+//         return $arr;
+//     }
+//     return [];
+// }
+
+function getProductCategories($id)
+{
+    global $db;
+    $query = $db->query("SELECT * FROM tbl_category INNER JOIN tbl_product_category ON tbl_category.id_category = tbl_product_category.id_category WHERE id_product = '$id'");
+    if ($query->num_rows) {
+        return $query;
+    }
+    return null;
+}
+
+function updateProduct($id, $name, $slug, $price, $short_des, $long_des, $image, $new_id_categories)
+{
+    // old [2, 5];
+    // new [1, 2, 3]
+
+    $product = getProductByID($id);
+    $image_path = $product->image;
+
+    if ($image['error'] === 0) {
+        $image_path = uploadProductImage($image);
+        if ($image_path) {
+            unlink($product->image);
+        }
+    }
+
+    global $db;
+    $db->begin_transaction();
+    try {
+
+        $old_id_categories = [];
+        $product_categories = getProductCategories($id);
+        while ($row = $product_categories->fetch_object()) {
+            $old_id_categories[] = $row->id_category;
+        }
+
+
+        $db->query("UPDATE tbl_product SET name = '$name', slug = '$slug', price = '$price', short_des = '$short_des', long_des = '$long_des', image = '$image_path' WHERE id_product = '$id'");
+
+        foreach ($old_id_categories as $old_id_category) {
+            if (!in_array($old_id_category, $new_id_categories)) {
+                $db->query("DELETE FROM tbl_product_category WHERE id_product = '$id' AND id_category = '$old_id_category'");
+            }
+        }
+        foreach ($new_id_categories as $new_id_category) {
+            if (!in_array($new_id_category, $old_id_categories)) {
+                $db->query("INSERT INTO tbl_product_category (id_product, id_category) VALUES ('$id', '$new_id_category')");
+            }
+        }
+
+        $db->commit();
+        return getProductByID($id);
+    } catch (Exception $e) {
+        $db->rollback();
+        return false;
+    }
+}
+
+
+function uploadProductImage($image)
+{
+    $img_name = $image['name'];
+    $img_size = $image['size'];
+    $tmp_name = $image['tmp_name'];
+    $error = $image['error'];
+
+    $dir = './assets/images/';
+
+    $allow_exs = ['pjg', 'png', 'jpeg'];
+    if ($error !== 0) {
+        throw new Exception('Unknown error occurred!');
+    }
+
+    if ($img_size > 500000) {
+        throw new Exception('File size is too large!');
+    }
+
+    $image_ex = pathinfo($img_name, PATHINFO_EXTENSION);
+    $image_lowercase_ex = strtolower($image_ex);
+
+    if (!in_array($image_lowercase_ex, $allow_exs)) {
+        throw new Exception('File extension is not allowed!');
+    }
+
+    $new_image_name = uniqid("PI-") . '.' . $image_lowercase_ex;
+    $image_path = $dir . $new_image_name;
+    move_uploaded_file($tmp_name, $image_path);
+    return $image_path; //string
+}
--- pages/product/home.php
+++ pages/product/home.php
@@ -14,6 +14,7 @@
                     <th>Quantity</th>
                     <th>Short Des</th>
                     <th>Image</th>
+                    <th>Category</th>
                 </tr>
                 <?php
                 $manage_products = getProducts();
@@ -28,6 +29,18 @@
                             <td><?php echo $row->qty ?></td>
                             <td><?php echo $row->short_des ?></td>
                             <td><img style="width : 100px" src="<?php echo $row->image ?>" alt=""></td>
+                            <td>
+                                <?php
+                                $categories = getProductCategories($row->id_product);
+                                if ($categories !== null) {
+                                    while ($category = $categories->fetch_object()) {
+                                        echo $category->name . '<br>';
+                                    }
+                                } else {
+                                    echo 'No categories';
+                                }
+                                ?>
+                            </td>
                             <td>
                                 <a class="btn btn-primary" href="./?page=product/update&id=<?php echo $row->id_product ?>">update</a>
                                 <a class="btn btn-danger" href="./?page=product/delete&id=<?php echo $row->id_product ?>">delete</a>
